6642343154c2b8b70ae5b393013de13673325c92
Zach <104291766+zachary-o@users.noreply.github.com>
2024-08-18 17:36:08 +0700

    empty cart UI

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -1,15 +1,15 @@
-import { findOrCreateCart, updateCartTotalAmount } from "@/lib"
-import { CreateCartItemValues } from "@/services/dto/cart.dto"
-import crypto from "crypto"
-import { NextRequest, NextResponse } from "next/server"
-import { prisma } from "../../../../prisma/prisma-client"
+import { findOrCreateCart, updateCartTotalAmount } from "@/lib";
+import { CreateCartItemValues } from "@/services/dto/cart.dto";
+import crypto from "crypto";
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "../../../../prisma/prisma-client";
 
 export async function GET(req: NextRequest) {
   try {
-    const token = req.cookies.get("cartToken")?.value
+    const token = req.cookies.get("cartToken")?.value;
 
     if (!token) {
-      return NextResponse.json({ totalAmount: 0, items: [] })
+      return NextResponse.json({ totalAmount: 0, items: [] });
     }
 
     const userCart = await prisma.cart.findFirst({
@@ -31,29 +31,29 @@ export async function GET(req: NextRequest) {
           },
         },
       },
-    })
-    return NextResponse.json(userCart)
+    });
+    return NextResponse.json(userCart);
   } catch (error) {
-    console.log("[CART_GET] Server error", error)
+    console.log("[CART_GET] Server error", error);
     return NextResponse.json(
       { message: "Failed to get cart item(s)" },
       { status: 500 }
-    )
+    );
   }
 }
 
 export async function POST(req: NextRequest) {
   try {
-    let token = req.cookies.get("cartToken")?.value
+    let token = req.cookies.get("cartToken")?.value;
 
     if (!token) {
-      token = crypto.randomUUID()
+      token = crypto.randomUUID();
     }
 
-    const userCart = await findOrCreateCart(token)
-    console.log('userCart', userCart)
+    const userCart = await findOrCreateCart(token);
+    console.log("userCart", userCart);
 
-    const data = (await req.json()) as CreateCartItemValues
+    const data = (await req.json()) as CreateCartItemValues;
 
     const findCartItem = await prisma.cartItem.findFirst({
       where: {
@@ -67,7 +67,7 @@ export async function POST(req: NextRequest) {
           },
         },
       },
-    })
+    });
 
     // If the same product was found, add +1
     if (findCartItem) {
@@ -78,7 +78,7 @@ export async function POST(req: NextRequest) {
         data: {
           quantity: findCartItem.quantity + 1,
         },
-      })
+      });
     }
 
     await prisma.cartItem.create({
@@ -90,19 +90,18 @@ export async function POST(req: NextRequest) {
           connect: data.ingredients?.map((id) => ({ id })),
         },
       },
-    })
+    });
 
-    const updatedUserCart = await updateCartTotalAmount(token)
+    const updatedUserCart = await updateCartTotalAmount(token);
 
-    const resp = NextResponse.json(updatedUserCart)
-    resp.cookies.set("cartToken", token)
-    return resp
-    
+    const resp = NextResponse.json(updatedUserCart);
+    resp.cookies.set("cartToken", token);
+    return resp;
   } catch (error) {
-    console.log("[CART_POST] Server error", error)
+    console.log("[CART_POST] Server error", error);
     return NextResponse.json(
       { message: "Failed to create cart" },
       { status: 500 }
-    )
+    );
   }
 }
--- src/components/shared/cart-drawer.tsx
+++ src/components/shared/cart-drawer.tsx
@@ -1,12 +1,16 @@
 "use client";
 
+import { PizzaSize, PizzaType } from "@/constants/pizza";
+import { getCartItemDetails } from "@/lib";
 import { useCartStore } from "@/store";
-import { ArrowRight } from "lucide-react";
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import { PropsWithChildren, useEffect } from "react";
 import { Button } from "../ui";
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetFooter,
   SheetHeader,
@@ -14,8 +18,8 @@ import {
   SheetTrigger,
 } from "../ui/sheet";
 import { CartDrawerItem } from "./cart-drawer-item";
-import { getCartItemDetails } from "@/lib";
-import { PizzaSize, PizzaType } from "@/constants/pizza";
+import { Title } from "./title";
+import { cn } from "@/lib/utils";
 
 interface Props {
   className?: string;
@@ -57,60 +61,95 @@ export const CartDrawer: React.FC<PropsWithChildren<Props>> = ({
     <Sheet>
       <SheetTrigger asChild>{children}</SheetTrigger>
       <SheetContent className="flex flex-col justify-between pb-0 bg-[#f4f1ee]">
-        <SheetHeader>
-          <SheetTitle>
-            <span className="fond-bold">
-              {items.length} {items.length === 1 ? "item" : "items"}
-            </span>{" "}
-            in cart
-          </SheetTitle>
-        </SheetHeader>
+        <div
+          className={cn(
+            "flex flex-col h-full",
+            !totalAmount && "justify-center"
+          )}
+        >
+          {totalAmount > 0 && (
+            <SheetHeader>
+              <SheetTitle>
+                <span className="fond-bold">
+                  {items.length} {items.length === 1 ? "item" : "items"}
+                </span>{" "}
+                in cart
+              </SheetTitle>
+            </SheetHeader>
+          )}
 
-        <div className="-mx-6 mt-5 overflow-auto flex-1">
-          {items.map((item) => (
-            <div className="mb-2" key={item.id}>
-              <CartDrawerItem
-                id={item.id}
-                imageUrl={item.imageUrl}
-                name={item.name}
-                details={
-                  item.pizzaSize && item.pizzaType
-                    ? getCartItemDetails(
-                        item.pizzaType as PizzaType,
-                        item.pizzaSize as PizzaSize,
-                        item.ingredients
-                      )
-                    : ""
-                }
-                price={item.price}
-                quantity={item.quantity}
-                disabled={item.disabled}
-                onClickCountButton={(type) =>
-                  onClickCountButton(item.id, item.quantity, type)
-                }
-                onClickRemoveCartItem={() => removeCartItem(item.id)}
+          {!totalAmount && (
+            <div className="flex flex-col items-center justify-center w-72 mx-auto">
+              <Image
+                src="/assets/images/empty-box.png"
+                alt="Empty cart"
+                width={120}
+                height={120}
               />
-            </div>
-          ))}
-        </div>
+              <Title
+                className="text-center font-bold my-2"
+                text="Your cart is empty"
+              />
 
-        <SheetFooter className="-mx-6 bg-white p-8">
-          <div className="w-full">
-            <div className="flex mb-4">
-              <span className="flex flex-1 text-lg text-neutral-500">
-                Total
-                <div className="flex-1 border-b border-dashed border-b-neutral-200 relative -top-1 mx-2" />
-              </span>
-              <span className="font-bold text-lg">${totalAmount}</span>
+              <SheetClose>
+                <Button className="w-56 h-12 text-base" size="lg">
+                  <ArrowLeft className="w-5 mr-2" />
+                  Back
+                </Button>
+              </SheetClose>
             </div>
-            <Link href="/cart">
-              <Button className="w-full h-12 text-base" type="submit">
-                Order
-                <ArrowRight className="w-5 ml-2" />
-              </Button>
-            </Link>
-          </div>
-        </SheetFooter>
+          )}
+
+          {totalAmount > 0 && (
+            <>
+              <div className="-mx-6 mt-5 overflow-auto flex-1">
+                {items.map((item) => (
+                  <div className="mb-2" key={item.id}>
+                    <CartDrawerItem
+                      id={item.id}
+                      imageUrl={item.imageUrl}
+                      name={item.name}
+                      details={
+                        item.pizzaSize && item.pizzaType
+                          ? getCartItemDetails(
+                              item.pizzaType as PizzaType,
+                              item.pizzaSize as PizzaSize,
+                              item.ingredients
+                            )
+                          : ""
+                      }
+                      price={item.price}
+                      quantity={item.quantity}
+                      disabled={item.disabled}
+                      onClickCountButton={(type) =>
+                        onClickCountButton(item.id, item.quantity, type)
+                      }
+                      onClickRemoveCartItem={() => removeCartItem(item.id)}
+                    />
+                  </div>
+                ))}
+              </div>
+
+              <SheetFooter className="-mx-6 bg-white p-8">
+                <div className="w-full">
+                  <div className="flex mb-4">
+                    <span className="flex flex-1 text-lg text-neutral-500">
+                      Total
+                      <div className="flex-1 border-b border-dashed border-b-neutral-200 relative -top-1 mx-2" />
+                    </span>
+                    <span className="font-bold text-lg">${totalAmount}</span>
+                  </div>
+                  <Link href="/cart">
+                    <Button className="w-full h-12 text-base" type="submit">
+                      Order
+                      <ArrowRight className="w-5 ml-2" />
+                    </Button>
+                  </Link>
+                </div>
+              </SheetFooter>
+            </>
+          )}
+        </div>
       </SheetContent>
     </Sheet>
   );
