6e288637cb8d76590dea92d3d10698c1a97a6a66
Zach <104291766+zachary-o@users.noreply.github.com>
2024-08-19 00:58:27 +0700

    checkout page

---
+++ src/app/(checkout)/checkout/page.tsx
@@ -0,0 +1,13 @@
+import { Title } from "@/components/shared";
+
+interface Props {
+  className?: string;
+}
+
+const Checkout: React.FC<Props> = ({ className }) => {
+  return <div>
+    <Title text="jjjgjgjgj"/>
+  </div>;
+};
+
+export default Checkout;
--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -9,12 +9,18 @@ import { Container } from "./container";
 import { SearchInput } from "./search-input";
 
 interface Props {
+  hasSearch?: boolean;
+  hasCart?: boolean;
   className?: string;
 }
 
-export const Header: React.FC<Props> = ({ className }) => {
+export const Header: React.FC<Props> = ({
+  hasSearch = true,
+  hasCart = true,
+  className,
+}) => {
   return (
-    <header className={cn("border border-b", className)}>
+    <header className={cn("border-b", className)}>
       <Container className="flex items-center justify-between py-8">
         {/* LEFT SIDE */}
         <Link href="/">
@@ -29,9 +35,11 @@ export const Header: React.FC<Props> = ({ className }) => {
           </div>
         </Link>
 
-        <div className="mx-10 flex-1">
-          <SearchInput />
-        </div>
+        {hasSearch && (
+          <div className="mx-10 flex-1">
+            <SearchInput />
+          </div>
+        )}
 
         {/* RIGHT SIDE */}
         <div className="flex items-center gap-1">
@@ -40,9 +48,11 @@ export const Header: React.FC<Props> = ({ className }) => {
             Login
           </Button>
 
-          <div>
-            <CartButton />
-          </div>
+          {hasCart && (
+            <div>
+              <CartButton />
+            </div>
+          )}
         </div>
       </Container>
     </header>
