97c1628a977df209ef8ff9ca87e5871c3c5899c3
Zach <104291766+zachary-o@users.noreply.github.com>
2024-09-02 15:53:11 +0700

    fix bugs

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,7 +3,7 @@ import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
-import { Loader2 } from "lucide-react";
+import { Loader } from "lucide-react";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md active:translate-y-[1px] text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 disabled:bg-gray-500",
@@ -62,7 +62,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         ref={ref}
         {...props}
       >
-        {!loading ? children : <Loader2 className="w-5 h-5 animate-spin" />}
+        {!loading ? children : <Loader className="w-5 h-5 animate-spin" />}
       </Comp>
     );
   }
